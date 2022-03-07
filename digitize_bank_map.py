d00f50def5180c26a78a302d6505caf5461218d7
clinton0313 <clinton0313@gmail.com>
2022-03-07 19:58:23 +0100

    Clipped raster file.
    Digitized raster file.
    
    Added vectorizer methods.

---
+++ digitize_bank_map.py
@@ -0,0 +1,79 @@
+#%%
+import geopandas as gpd
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import osgeo.gdal as gdal
+import rasterio
+
+
+from affine import Affine
+from rasterio.mask import mask
+from shapely.geometry import mapping, MultiPolygon
+from vectorize import gdf_to_graph, array2shp, raster2array, pixelOffset2coord, path_to_edges
+
+ADM_PATH = "ESP_adm"
+DATAPATH = "data"
+PROCESSEDPATH = os.path.join(DATAPATH, "processed")
+EPSG = 32630
+CLIPPED_FN = "spanish_map_clipped.tif"
+DIGITIZED_FN = "spanish_map_digitized.tif"
+SHAPE_FILE = "spanish_map_vectorized.shp"
+mainland_bounds = (-0.2e6, 1.5e6, 3.7e6, 5.1e6)
+
+#%% Clipping the map
+bank_map = rasterio.open(os.path.join(DATAPATH, "spanish_map_georeferenced_epsg32630.tif"), "r")
+adm_data = gpd.read_file(os.path.join(ADM_PATH, "ESP_adm4.shp"))
+adm_data = adm_data.to_crs(epsg=EPSG).cx[mainland_bounds[0]:mainland_bounds[1], mainland_bounds[2]:mainland_bounds[3]]
+spain = adm_data.unary_union
+shape = [mapping(spain)]
+
+out_img, out_transform = mask(bank_map, shapes=shape)
+out_meta = bank_map.meta.copy()
+
+with rasterio.open(os.path.join(PROCESSEDPATH, CLIPPED_FN), "w", **out_meta) as outfile:
+    outfile.write(out_img)
+#%%
+#Digitizing the map
+
+thresholds = [240., 240., 240.]
+bins = [np.array([0., threshold]) for threshold in thresholds]
+
+bank_tif = gdal.Open(os.path.join(PROCESSEDPATH, CLIPPED_FN), gdal.GA_ReadOnly)
+bank_transform = bank_tif.GetGeoTransform()
+rasterbands = [bank_tif.GetRasterBand(i).ReadAsArray() for i in range(1,4)]
+
+digitized_bands = [1 - (np.digitize(band, bins =bin ) - 1) for band, bin in zip(rasterbands, bins)]
+digitized = digitized_bands[0] * digitized_bands[1] * digitized_bands[2]
+digitized = np.float32(digitized) * 255
+
+fig, ax = plt.subplots(figsize=(26,26))
+ax.imshow(digitized, cmap="Greys")
+
+#%%
+#Save raster to file
+
+with rasterio.open(
+    os.path.join(PROCESSEDPATH, DIGITIZED_FN), 'w',
+    driver="GTiff",
+    height=digitized.shape[0],
+    width=digitized.shape[1],
+    count=1,
+    dtype=np.float32,
+    crs=f"EPSG:{EPSG}",
+    transform=Affine.from_gdal(*bank_transform)
+) as outfile:
+    outfile.write(digitized, 1)
+outfile.close()
+
+# %%
+array = raster2array(os.path.join(DATAPATH, DIGITIZED_FN))
+array2shp(
+    array=array, 
+    outSHPfn=os.path.join(DATAPATH, SHAPE_FILE), 
+    rasterfn=os.path.join(DATAPATH, DIGITIZED_FN), 
+    pixelValue=255
+    )
+
+# %%
--- load_bank_data.py
+++ load_bank_data.py
@@ -1,11 +1,13 @@
 #%%
 import geopandas as gpd
 import matplotlib.pyplot as plt
+import numpy as np
 import os
 import pandas as pd
 import regex as re
 import rasterio as rio
 
+from osgeo import gdal
 from rasterio.plot import show
 from shapely.geometry import Point, mapping
 
@@ -15,23 +17,25 @@ os.chdir(os.path.dirname(os.path.realpath(__file__)))
 ADM_PATH = "ESP_adm"
 DATAPATH = "data"
 EPSG = 32630
-mainland_bounds = (0.3e6, 1.5e6, 3.7e6, 5.1e6)
+mainland_bounds = (-0.2e6, 1.5e6, 3.7e6, 5.1e6)
 
 #%%
 
-adm_data = gpd.read_file(os.path.join(ADM_PATH, "ESP_adm3.shp"))
+adm_data = gpd.read_file(os.path.join(ADM_PATH, "ESP_adm4.shp"))
 bank_data = pd.read_csv(os.path.join(DATAPATH, "municipalities_data.csv"))
 
 bank_data["geometry"] = bank_data.apply(lambda x: Point(x["LONGITUD_ETRS89"], x["LATITUD_ETRS89"]), axis=1)
 bank_data = gpd.GeoDataFrame(data=bank_data, geometry="geometry", crs="ETRS89")
 bank_data = bank_data.to_crs(epsg=EPSG).cx[mainland_bounds[0]:mainland_bounds[1], mainland_bounds[2]:mainland_bounds[3]]
 adm_data = adm_data.to_crs(epsg=EPSG).cx[mainland_bounds[0]:mainland_bounds[1], mainland_bounds[2]:mainland_bounds[3]]
-bank_map = rio.open(os.path.join(DATAPATH, "spanish_map_georeferenced_epsg32630.tif"))
+bank_map = rio.open(os.path.join(DATAPATH, "spanish_map_digitized.tif"))
+
+
 
 # %%
 fig, ax = plt.subplots(figsize=(14,14))
 show(bank_map, ax=ax)
-adm_data.plot(ax=ax)
-bank_data.plot(ax=ax, column="treatment", markersize=10, legend=True)
+# adm_data.plot(ax=ax)
+# bank_data.plot(ax=ax, column="treatment", markersize=10, legend=True)
 
 # %%

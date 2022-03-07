Fixed masking to come after digitizing and to properly account for nodata

digitize_bank_map.py
@@ -22,27 +22,17 @@ DIGITIZED_FN = "spanish_map_digitized.tif"
 SHAPE_FILE = "spanish_map_vectorized.shp"
 mainland_bounds = (-0.2e6, 1.5e6, 3.7e6, 5.1e6)
 
-#%% Clipping the map
-bank_map = rasterio.open(os.path.join(DATAPATH, "spanish_map_georeferenced_epsg32630.tif"), "r")
-adm_data = gpd.read_file(os.path.join(ADM_PATH, "ESP_adm4.shp"))
-adm_data = adm_data.to_crs(epsg=EPSG).cx[mainland_bounds[0]:mainland_bounds[1], mainland_bounds[2]:mainland_bounds[3]]
-spain = adm_data.unary_union
-shape = [mapping(spain)]
-
-out_img, out_transform = mask(bank_map, shapes=shape)
-out_meta = bank_map.meta.copy()
-
-with rasterio.open(os.path.join(PROCESSEDPATH, CLIPPED_FN), "w", **out_meta) as outfile:
-    outfile.write(out_img)
 #%%
 #Digitizing the map
 
 thresholds = [240., 240., 240.]
 bins = [np.array([0., threshold]) for threshold in thresholds]
 
-bank_tif = gdal.Open(os.path.join(PROCESSEDPATH, CLIPPED_FN), gdal.GA_ReadOnly)
+bank_tif = gdal.Open(os.path.join(DATAPATH, "spanish_map_georeferenced_epsg32630.tif"), gdal.GA_ReadOnly)
 bank_transform = bank_tif.GetGeoTransform()
 rasterbands = [bank_tif.GetRasterBand(i).ReadAsArray() for i in range(1,4)]
+
+#%%
 
 digitized_bands = [1 - (np.digitize(band, bins =bin ) - 1) for band, bin in zip(rasterbands, bins)]
 digitized = digitized_bands[0] * digitized_bands[1] * digitized_bands[2]
@@ -67,12 +57,26 @@ with rasterio.open(
     outfile.write(digitized, 1)
 outfile.close()
 
+#%% Clipping the map
+bank_map = rasterio.open(os.path.join(PROCESSEDPATH, DIGITIZED_FN), "r")
+adm_data = gpd.read_file(os.path.join(ADM_PATH, "ESP_adm4.shp"))
+adm_data = adm_data.to_crs(epsg=EPSG).cx[mainland_bounds[0]:mainland_bounds[1], mainland_bounds[2]:mainland_bounds[3]]
+spain = adm_data.unary_union
+shape = [mapping(spain)]
+
+out_img, out_transform = mask(bank_map, shapes=shape)
+out_meta = bank_map.meta.copy()
+out_meta["nodata"] = np.nan
+
+with rasterio.open(os.path.join(PROCESSEDPATH, CLIPPED_FN), "w", **out_meta) as outfile:
+    outfile.write(out_img)
+
 # %%
-array = raster2array(os.path.join(DATAPATH, DIGITIZED_FN))
+array = raster2array(os.path.join(PROCESSEDPATH, CLIPPED_FN))
 array2shp(
     array=array, 
-    outSHPfn=os.path.join(DATAPATH, SHAPE_FILE), 
-    rasterfn=os.path.join(DATAPATH, DIGITIZED_FN), 
+    outSHPfn=os.path.join(PROCESSEDPATH, SHAPE_FILE), 
+    rasterfn=os.path.join(PROCESSEDPATH, CLIPPED_FN), 
     pixelValue=255
     )
 
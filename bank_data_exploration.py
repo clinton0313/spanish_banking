680523a982724268762effdbe159abe32aa604c0
clinton0313 <clinton0313@gmail.com>
2022-03-12 10:01:00 +0100

    Spatially lagged municipality data computed.

--- bank_data_exploration.py
+++ bank_data_exploration.py
@@ -23,21 +23,31 @@ bank_data = gpd.GeoDataFrame(data=bank_data, geometry="geometry", crs="ETRS89")
 bank_data = bank_data.to_crs(epsg=EPSG).cx[mainland_bounds[0]:mainland_bounds[1], mainland_bounds[2]:mainland_bounds[3]]
 
 #%%
-def lag_year(data:pd.DataFrame, year:Union[float, int], x_cols:list, year_col:str = "year"):
+def lag_year(data:pd.DataFrame, kernels:list, bandwidths:list, year:Union[float, int], x_cols:list, year_col:str = "year"):
     yearly_data = data[data[year_col] == year].reset_index(drop=True)
-    w = weights.Kernel.from_dataframe(yearly_data, function="gaussian", fixed=False, k=20)
-    w = fill_diagonal(w, 0)
-    w.transform = 'r'
-    wX = weights.spatial_lag.lag_spatial(w, yearly_data[x_cols].values.astype(float))
-    lagged_data = pd.DataFrame(wX, columns = [f"lag_{name}" for name in x_cols])
-    combined_data = pd.concat((yearly_data, lagged_data), axis=1)
+    combined_data = yearly_data.copy()
+    for kernel in kernels:
+        for bandwidth in bandwidths:
+            w = weights.Kernel.from_dataframe(yearly_data, function=kernel, fixed=True, bandwidth=bandwidth, k=50)
+            w = fill_diagonal(w, 0)
+            w.transform = 'r'
+            wX = weights.spatial_lag.lag_spatial(w, yearly_data[x_cols].values.astype(float))
+            lagged_data = pd.DataFrame(wX, columns = [f"lag_{kernel}_{bandwidth//1000}_{name}" for name in x_cols])
+            combined_data = pd.concat((combined_data, lagged_data), axis=1)
     return combined_data
 
-def add_lags(data: pd.DataFrame, x_cols:list, year_col:str = "year"):
-    lagged_bank_data = [lag_year(data, year, x_cols, year_col) for year in data[year_col].unique()]
+def add_lags(data: pd.DataFrame, kernels:list, bandwidths:list, x_cols:list, year_col:str = "year"):
+    lagged_bank_data = [lag_year(data, kernels, bandwidths, year, x_cols, year_col) for year in data[year_col].unique()]
     return pd.concat(lagged_bank_data, axis=0)
 #%%
-lagged_bank_data = add_lags(bank_data, ["num_commercial_banks", "num_savings_banks", "num_commercial_banks_p1000c", "comm_lic_1", "comm_lic_1_p1000c"])
+kernels = ["triangular", "gaussian"]
+bandwidths = range(5000, 40000, 5000)
+lagged_bank_data = add_lags(
+    bank_data, 
+    kernels, 
+    bandwidths, 
+    ["num_commercial_banks", "num_savings_banks", "num_commercial_banks_p1000c", "comm_lic_1", "comm_lic_1_p1000c"]
+)
 lagged_bank_data.to_csv(os.path.join(DATAPATH, "lagged_municipalities_w_dist.csv"))
 
 #%%
